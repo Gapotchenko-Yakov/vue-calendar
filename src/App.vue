bb95409302e4092382707f0c8a306c292c65a8d0
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-21 17:19:00 +0300

    feat(LangSwitcher): add LangSwitcher button for toggling language

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,25 @@
-<script setup lang="ts">
-import Calendar from './components/Calendar.vue'
-</script>
-
 <template>
-  <calendar />
+  <lang-switcher 
+    :model-value="lang"
+    @update:model-value="val => lang = val"
+    class="lang-switcher"
+  />
+  <calendar 
+    :locale="lang"
+  />
 </template>
 
-<style scoped>
+<script setup lang="ts">
+import { ref } from 'vue';
+import Calendar from './components/Calendar.vue'
+import LangSwitcher from './components/LangSwitcher.vue'
+import type { Language } from './types/locales';
 
+const lang = ref<Language>('en');
+</script>
+
+<style scoped>
+  .lang-switcher {
+    margin-bottom: 12px;
+  }
 </style>
---
+++ src/components/LangSwitcher.vue
@@ -0,0 +1,32 @@
+<template>
+    <button
+        @click="switchLanguage"
+        class="lang-switcher"
+    >
+        {{ lang.toLocaleUpperCase() }}
+    </button>
+</template>
+
+<script setup lang="ts">
+import type { Language } from '../types/locales';
+
+interface LangSwitcherProps {
+    modelValue: Language;
+}
+
+const { modelValue: lang } = defineProps<LangSwitcherProps>();
+const emit = defineEmits(['update:modelValue'])
+
+    function switchLanguage(){
+        const newLang = lang === 'en' ? 'ru' : 'en';
+        emit('update:modelValue', newLang);
+    }
+
+</script>
+
+<style scoped>
+.lang-switcher {
+    width: 100%;
+    border: gray solid 1px;
+}
+</style>
