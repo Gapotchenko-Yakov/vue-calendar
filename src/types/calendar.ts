65170eae715c4a394cbec5f79707349f2b5dcdb2
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-22 00:13:12 +0300

    chore(Calendar): add Calendar types

---
+++ src/types/calendar.ts
@@ -0,0 +1,7 @@
+
+type CalendarYear = `${number}${number}${number}${number}`;
+type CalendarMonth = `${number}${number}`;
+type CalendarDate = `${number}${number}`;
+
+
+export type CalendarDateString = `${CalendarYear}-${CalendarMonth}-${CalendarDate}`;
