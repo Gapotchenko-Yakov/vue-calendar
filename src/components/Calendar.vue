dd6bb858fc37c11e0a545deb4484848fd1fbcb3c
Yakov-Gapotchenko <yakov.gapotchenko@gmail.com>
2025-08-21 14:44:36 +0300

    feat(Calendar): add minimal working Calendar component
    - basic structure with month navigation
    - handles selected date
    - emits 'update:modelValue' and 'select'

---
+++ src/components/Calendar.vue
@@ -0,0 +1,144 @@
+<template>
+    <div class="container">
+        <div class="month-year">
+            <button
+                v-if="displayedMonth !== null"
+                @click="decreaseMonth"
+                :disabled="displayedMonth <= 0"
+            >◀</button>
+            <span>
+                {{ displayedMonth }} {{ displayedYear }}
+            </span>
+            <button
+                v-if="displayedMonth !== null"
+                @click="increaseMonth"
+                :disabled="displayedMonth >= 11"
+            >▶</button>
+        </div>
+        <div class="weekdays">
+            <span
+                v-for="weekday in weekdaysShort"
+                :key="weekday"
+                class="weekday"
+            >
+                {{ weekday }}
+            </span>
+        </div>
+        <div class="calendar-grid">
+            <button
+                v-if="displayedYear !== null && displayedMonth !== null && selectedDate !== null "
+                v-for="day in getDaysInMonth(displayedYear, displayedMonth)"
+                @click="changeDate(day)"
+                :class="formatDate(new Date(displayedYear, displayedMonth, day)) === formatDate(selectedDate) ? 'day__selected' : null"
+                >
+                {{ day }}
+            </button>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+    const {
+        modelValue,
+    } = defineProps({
+        modelValue: {
+            type: Date,
+        },
+    });
+
+    const emit = defineEmits(['update:modelValue', 'select']);
+
+    const selectedDate = ref<Date | null>(null);
+    const displayedMonth = ref<number | null>(null);
+    const displayedYear = ref<number | null>(null);
+
+    if(modelValue){
+        selectedDate.value = modelValue;
+        displayedMonth.value = modelValue.getMonth();
+        displayedYear.value = modelValue.getFullYear();
+    } else {
+        const today = new Date();
+        selectedDate.value = today;
+        emit('update:modelValue', today);
+        displayedMonth.value = today.getMonth();
+        displayedYear.value = today.getFullYear();
+    }
+
+    function getDaysInMonth(year: number, month: number): number{
+        return new Date(year, month + 1, 0).getDate();
+    }
+
+    function formatDate(date: Date) {
+        return `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}`;
+    }
+    
+    const weekdaysShort = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
+
+    function changeDate(day: number) {
+        if(displayedYear.value !== null && displayedMonth.value !== null){
+            const date = new Date(displayedYear.value, displayedMonth.value, day);
+            selectedDate.value = date;
+            emit('update:modelValue', date);
+            emit('select', date);
+        }
+    }
+
+    function increaseMonth(){
+        if(displayedMonth.value !== null && displayedYear.value !== null){
+            if(displayedMonth.value <=10)
+                displayedMonth.value++;
+            else {
+                displayedYear.value++;
+                displayedMonth.value = 0;
+            }
+        }
+    }
+
+    function decreaseMonth(){
+        if(displayedMonth.value !== null && displayedYear.value !== null){
+            if(displayedMonth.value >= 1)
+                displayedMonth.value--;
+            else {
+                displayedYear.value--;
+                displayedMonth.value = 11;
+            }
+        }
+    }
+</script>
+
+<style scoped>
+ .container {
+    display: flex;
+    flex-direction: column;
+    min-width: 240px;
+    height: 320px;
+ }
+
+  .month-year {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+ }
+
+ .weekdays {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+ }
+
+ .calendar-grid {
+    background-color: #ccc;
+    display: grid;
+    grid-template-columns: repeat(7, 1fr);
+    gap: 4px;
+    width: 100%;
+    flex-grow: 1;
+    padding: 4px;
+ }
+
+ .day__selected {
+    background-color: blueviolet;
+ }
+</style>
